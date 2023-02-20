f999e562d7308a09737a13c24e8d4e128047d6fa
AndreyMir <miroshal@mail.ru>
2023-02-20 03:37:57 +0300

    Задача 13

---
+++ dz13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет. ");
+}
+else
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    a = a % 10;
+    Console.WriteLine(a);
+}
